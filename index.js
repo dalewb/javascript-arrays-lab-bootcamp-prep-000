130a1d5f2bd9a2f29861a574d78b55a9d4cc7782
William Dale <williambdale@gmail.com>
2018-02-08 22:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = "I don't do much." //You are a CONSTANT disappointment 
 
-function destructivelyAppendKitten(kitten) {
-  kittens.push(kitten)
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name)
